30a31cfd9d005480c487aeafebad1bce9f5c1f6b
kharioki <antolisah@gmail.com>
2020-07-13 23:15:34 +0300

    added product page, and home page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
 import { Client as Styletron } from 'styletron-engine-atomic';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { HomePage } from '../pages/HomePage';
+import ProductPage from '../pages/ProductPage';
+
 const debug =
   process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
 
@@ -9,11 +14,18 @@ const engine = new Styletron();
 
 function App() {
   return (
-    <StyletronProvider
-      value={engine}
-      debug={debug}
-      debugAfterHydration
-    ></StyletronProvider>
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <Router>
+        <Switch>
+          <Route path="/product">
+            <ProductPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </StyletronProvider>
   );
 }
 
