de21e2f907435c23c314220380ed6d0fa2d5a2fd
kharioki <antolisah@gmail.com>
2020-07-13 22:58:44 +0300

    styletron provider wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+
+const debug =
+  process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
+
+const engine = new Styletron();
 
 function App() {
-  return <div>test</div>;
+  return (
+    <StyletronProvider
+      value={engine}
+      debug={debug}
+      debugAfterHydration
+    ></StyletronProvider>
+  );
 }
 
 export default App;
