2b7970715edd3e6ffb9bc28e0879b7a7ced40de7
kharioki <antolisah@gmail.com>
2020-07-14 15:04:26 +0300

    displaying product on product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,22 +1,67 @@
 import React, { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container, Div, Button, Text, Row, Col } from 'atomize';
 
 import { ShopContext } from '../context/shopContext';
 
 const ProductPage = () => {
   let { id } = useParams();
 
-  const { fetchProductWithId, product } = useContext(ShopContext);
+  const { fetchProductWithId, product, addItemToCheckout } = useContext(
+    ShopContext
+  );
 
   useEffect(() => {
     fetchProductWithId(id);
-  }, [fetchProductWithId]);
+  }, [fetchProductWithId, id]);
 
+  if (!product.title) return <div>Loading product...</div>;
   return (
-    <div>
-      Product
-      {id}
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Div
+            bgImg={product.images[0].src}
+            shadow="3"
+            bgSize="contain"
+            bgRepeat="no-repeat"
+            w="100%"
+            bgPos="center center"
+            h="40rem"
+          />
+        </Col>
+        <Col>
+          <Text
+            tag="h1"
+            textColor="black500"
+            textWeight="300"
+            m={{ y: '2rem' }}
+          >
+            {product.title}
+          </Text>
+          <Text tag="h3" m={{ y: '2rem' }} textWeight="200">
+            KES {product.variants[0].price}
+          </Text>
+          <Text
+            tag="p"
+            textSize="paragraph"
+            textColor="gray900"
+            textWeight="200"
+          >
+            {product.description}
+          </Text>
+          <Button
+            rounded="0"
+            shadow="3"
+            bg="black500"
+            m={{ y: '2rem' }}
+            onClick={() => addItemToCheckout(product.variants[0].id, 1)}
+          >
+            Add To Cart
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
